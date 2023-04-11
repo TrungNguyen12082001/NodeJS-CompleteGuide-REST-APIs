ba7de61c3c46d20efd09c9d933675f284f387c7b
TrungNguyen <nguyentattrung1208@gmail.com>
2023-04-11 14:42:10 +0700

    Fetching Lists of Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,12 +1,24 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First Post", content: "This is the first post!" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/trung.png",
+        creator: {
+          name: "Trung",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  console.log(title, content);
   // Create post in db
   res.status(200).json({
     message: "Post created successfully",
