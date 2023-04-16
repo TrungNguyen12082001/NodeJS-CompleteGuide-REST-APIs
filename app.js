86b78ef0bfc0d741fd9b4f08856f3bb9215b0008
TrungNguyen <nguyentattrung1208@gmail.com>
2023-04-16 21:58:40 +0700

    376 (Adding Server Side Validation) and Setting Up a Post Model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
@@ -21,4 +22,11 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://TrungNguyen-NodeComplete:WkzTfrNMAvDP5RNw@cluster0.wjkvrnd.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
